673727f60be13e79282212fdcb689b1900a827a2
BrunoYoshioka <bruno.yuji1995@gmail.com>
2023-08-01 03:42:57 -0300

    Adição do banco H2, perfil ambiente de teste, JPA

--- src/main/java/com/bruno/sgfuncionario/entity/Funcionario.java
+++ src/main/java/com/bruno/sgfuncionario/entity/Funcionario.java
@@ -1,11 +1,18 @@
 package com.bruno.sgfuncionario.entity;
 
 import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.util.Objects;
 
+@Entity
 public class Funcionario implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String designacao;
