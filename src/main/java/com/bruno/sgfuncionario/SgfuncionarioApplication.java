ba7933a2122bf1a036913440ca6232d9b2ba741b
BrunoYoshioka <bruno.yuji1995@gmail.com>
2023-08-01 02:06:03 -0300

    Project created

---
+++ src/main/java/com/bruno/sgfuncionario/SgfuncionarioApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.sgfuncionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgfuncionarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgfuncionarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bruno/sgfuncionario/SgfuncionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.sgfuncionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgfuncionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
